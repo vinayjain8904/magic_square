7a089474851189675e1f8ce35ae5a386574d88ff
Vinay <vinayjain8904@gmail.com>
2018-09-12 12:39:38 +0530

    magic square

---
+++ magic.py
@@ -0,0 +1,19 @@
+
+n = int(input(''))
+a =  [ [ 0 for j in range(n) ]  for i in range(n) ]
+
+i = int(0)
+j = int(n/2)
+
+for num in range(1,n*n+1):
+    a[i][j] = num
+    if num % n == 0 :
+        i += 1
+    else:
+        i -= 1
+        j += 1
+        if i<0 : i = n-1
+        if j==n : j = 0
+
+for xx in a:
+    print(xx)
